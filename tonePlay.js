ebec4901fc283af8e31e5f934d4eb1fa0a47f0ed
hodgek-py <khodge1@hotmail.com>
2022-06-29 22:37:48 -0600

    small changes

--- tonePlay.js
+++ tonePlay.js
@@ -1,25 +1,103 @@
 $(document).ready(function () {
 const Tone = window.Tone
 console.log(Tone);
-console.log("heyyy");
-$("#playTone").click(function (e) { 
-    e.preventDefault();
-    const synth = new Tone.PolySynth(Tone.FMSynth).toDestination();
+const aminor = ["A","B","C","D","E","F","G"]
+const cmajor = ["C","D","E","F","G","A","B"]
+const gmajor = ["G","A","B","C","D","E","F#"]
+const eminor = ["E", "F#", "G", "A","B","C","D"]
+const dmajor = ["D", "E", "F#", "G", "A", "B", "C#"]
+const bminor = ["B", "C#","D", "E", "F#", "G", "A"]
+
+var curKey
+var chordArray = []
+noteLen = ["2n","4n","8n","16n"]
+
+$("#playTone").click(function () { 
+    var chordArray = []
     const now = Tone.now()
     const sampler = new Tone.Sampler({
         urls: {
-            C1: "key.wav",
+            C3: "key.wav",
         },
         baseUrl: "./instruments/Keys/",
     }).toDestination();
 
-    Tone.loaded().then(() => {
-    sampler.triggerAttackRelease(["Eb1", "G1", "Bb1"], "1n")
-    })
+    
 
+    key = $("#MusicKey option:selected").val();
+    if (key == "Cmaj"){
+        curKey = cmajor
+    }
+    else if (key == "Amin"){
+        curKey = aminor
+    }
+
+
+    const synth = new Tone.PolySynth(Tone.MonoSynth).toDestination();
+
+
+    Tone.Transport.scheduleRepeat((time) => {
+        number = Math.floor(Math.random() * 3)
+        createChords(number,curKey)
+        sampler.triggerAttackRelease(chordArray, "1n", time);
+        //synth.triggerAttackRelease(chordArray, "2n", time);
+    }, "1m");
+    
+    Tone.Transport.scheduleRepeat((time) => {
+        number = Math.floor(Math.random() * 7)
+        length = Math.floor(Math.random() * 4)
+        Tone.loaded().then(() => {
+            sampler.triggerAttackRelease(curKey[number]+"3", noteLen[length], time)
+            })
+            
+        //synth.triggerAttackRelease(curKey[number]+"4", noteLen[length], time);
+    }, "4n", "2m");
+
+    
+
+
+
+    Tone.Transport.start();
+
+
+
+
+
+
+
+
+
+    function createChords(number, key) {
+        switch(number) {
+            case 0:
+              chordArray = [key[0]+"3",key[2]+"3",key[4]+"3"];
+              break;
+            case 1:
+                chordArray = [key[1]+"3",key[3]+"3",key[5]+"3"];
+              break;
+            case 2:
+              chordArray = [key[2]+"3",key[4]+"3",key[6]+"3"];
+              break;
+    }
+
+}
+
+
+
+
+
+
+$("#stopTone").click(function () { 
+    Tone.Transport.stop();
+});
 
 
 });
+
+
+
+
+
 
 
 
